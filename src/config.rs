1182a00e18c4d7481d6ef3d250e5f9dd37efcaf5
valeratrades <v79166789533@gmail.com>
2024-08-03 22:30:09 +0000

    feat: smart config and groups

--- src/config.rs
+++ src/config.rs
@@ -1,26 +1,173 @@
 use anyhow::Result;
-use serde::Deserialize;
-use v_utils::{io::ExpandedPath, macros::MyConfigPrimitives};
+use serde::{de, Deserialize, Deserializer, Serialize};
+use std::collections::BTreeMap; // so snapshot tests work
+use std::fmt;
+use std::path::Path;
+use std::str::FromStr;
+use v_utils::macros::MyConfigPrimitives;
 
 #[derive(Debug, Default, derive_new::new, Clone, MyConfigPrimitives)]
 pub struct AppConfig {
-	pub channels: Channels,
+	pub channels: BTreeMap<String, TelegramDestination>,
 }
-// In the perfect world would pick up the list of channels and their names from the config file, then create the cli bindings based on them. This feels redundant.
-#[derive(Default, Clone, derive_new::new, Debug, MyConfigPrimitives)]
-pub struct Channels {
-	pub wtt: isize,
-	pub journal: isize,
-	pub alerts: isize,
+
+#[derive(Clone, Debug, derive_new::new, Copy, PartialEq, Eq, Serialize)]
+/// Doesn't store "-100" prefix
+pub enum TelegramDestination {
+	Channel(u64),
+	Group { id: u64, thread_id: u64 },
+}
+impl TelegramDestination {
+	pub fn destination_params(&self) -> Vec<(&str, String)> {
+		match self {
+			Self::Channel(chat_id) => vec![("chat_id", format!("-100{chat_id}"))],
+			Self::Group { id, thread_id } => vec![("chat_id", format!("-100{id}")), ("message_thread_id", thread_id.to_string())],
+		}
+	}
+}
+
+impl<'de> Deserialize<'de> for TelegramDestination {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		struct TelegramChatVisitor;
+
+		impl<'de> de::Visitor<'de> for TelegramChatVisitor {
+			type Value = TelegramDestination;
+
+			fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+				formatter.write_str("a string or number representing a channel or group")
+			}
+
+			fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+			where
+				E: de::Error,
+			{
+				parse_telegram_destination_string(value).map_err(de::Error::custom)
+			}
+
+			fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+			where
+				E: de::Error,
+			{
+				Ok(TelegramDestination::Channel(value))
+			}
+
+			fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
+			where
+				E: de::Error,
+			{
+				parse_telegram_destination_string(&value.to_string()).map_err(de::Error::custom)
+			}
+		}
+
+		deserializer.deserialize_any(TelegramChatVisitor)
+	}
+}
+
+fn parse_telegram_destination_string(s: &str) -> Result<TelegramDestination, String> {
+	fn parse_chat_id(mut s: &str) -> Result<u64, String> {
+		s = s.trim_start_matches("-100");
+		s.parse::<u64>().map_err(|e| e.to_string())
+	}
+	if let Some((id_str, thread_id_str)) = s.split_once('/') {
+		let id = parse_chat_id(id_str)?;
+		let thread_id = u64::from_str(thread_id_str).map_err(|e| e.to_string())?;
+		Ok(TelegramDestination::Group { id, thread_id })
+	} else {
+		let id = parse_chat_id(s)?;
+		Ok(TelegramDestination::Channel(id))
+	}
 }
 
 impl AppConfig {
-	pub fn read(path: ExpandedPath) -> Result<Self, config::ConfigError> {
-		let builder = config::Config::builder().add_source(config::File::with_name(&path.to_string()));
+	pub fn read(path: &Path) -> Result<Self, config::ConfigError> {
+		let builder = config::Config::builder().add_source(config::File::with_name(&format!("{}", path.display())));
 
 		let settings: config::Config = builder.build()?;
 		let settings: Self = settings.try_deserialize()?;
 
 		Ok(settings)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use insta::assert_debug_snapshot;
+	use serde_json::from_str;
+
+	#[test]
+	fn test_deserialize_channel() {
+		let json = r#""2244305221""#;
+		let chat: TelegramDestination = from_str(json).unwrap();
+		assert_debug_snapshot!(chat, @r###"
+Channel(
+    2244305221,
+)
+"###);
+
+		let cases = [r#"2244305221"#, r#""-1002244305221""#, r#"-1002244305221"#];
+
+		for case in cases {
+			assert_eq!(from_str::<TelegramDestination>(case).unwrap(), chat);
+		}
+	}
+
+	#[test]
+	fn test_deserialize_group() {
+		let json = r#""2244305221/7""#;
+		let chat: TelegramDestination = from_str(json).unwrap();
+		assert_debug_snapshot!(chat, @r###"
+Group {
+    id: 2244305221,
+    thread_id: 7,
+}
+"###);
+	}
+
+	#[test]
+	fn test_deserialize_errors() {
+		let cases = [
+			(r#""invalid""#, "invalid digit found in string"),
+			(
+				r#"2244305221.5"#,
+				"invalid type: floating point `2244305221.5`, expected a string or number representing a channel or group",
+			),
+		];
+
+		for (json, expected_error) in cases {
+			let result: Result<TelegramDestination, _> = from_str(json);
+			assert!(result.is_err());
+			assert!(result.unwrap_err().to_string().contains(expected_error));
+		}
+	}
+
+	#[test]
+	fn test_deserialize_channels() {
+		let toml_str = r#"
+[channels]
+wtt = "2244305221"
+journal = "-1002244305222"
+alerts = "2244305223/7"
+"#;
+
+		let config: AppConfig = toml::from_str(toml_str).unwrap();
+
+		assert_debug_snapshot!(config.channels, @r###"
+  {
+      "alerts": Group {
+          id: 2244305223,
+          thread_id: 7,
+      },
+      "journal": Channel(
+          2244305222,
+      ),
+      "wtt": Channel(
+          2244305221,
+      ),
+  }
+  "###);
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{ArgGroup, Args, Parser, Subcommand};
-use reqwest::Error;
 use std::env;
 use v_utils::io::ExpandedPath;
+use anyhow::Result;
 mod config;
 
 #[derive(Parser)]
@@ -22,54 +22,42 @@ enum Commands {
 	Send(SendArgs),
 	/// Get information about the bot
 	BotInfo,
+	/// List all the channels defined in the config file
+	ListChannels,
 }
 #[derive(Args)]
-#[command(group(
-    ArgGroup::new("channel")
-        .required(true)
-        .args(&["wtt", "journal", "alerts"]),
-))]
 struct SendArgs {
-	/// Send message to WTT channel
+	/// Name of the channel to send the message to. Matches against the keys in the config file
 	#[arg(short, long)]
-	wtt: bool,
-	/// Send message to Journal channel
-	#[arg(short, long)]
-	journal: bool,
-	/// Send message to Alerts channel
-	#[arg(short, long)]
-	alerts: bool,
+	channel: String,
 
 	/// Message to send
 	message: Vec<String>,
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
 	let cli = Cli::parse();
-	let config = config::AppConfig::read(cli.config).expect("Failed to read config file");
+	let config = config::AppConfig::read(&cli.config.0).expect("Failed to read config file");
 	//TODO!: make it possible to define the bot_token inside the config too (env overwrites if exists)
 	let bot_token = env::var("TELEGRAM_BOT_KEY").expect("TELEGRAM_BOT_KEY not set");
 
 	match cli.command {
 		Commands::Send(args) => {
-			let chat_id: isize;
-			if args.wtt {
-				chat_id = config.channels.wtt; //"-1001179171854";
-			} else if args.journal {
-				chat_id = config.channels.journal; //"-1002128875937";
-			} else if args.alerts {
-				chat_id = config.channels.alerts; //"-1001800341082";
-			} else {
-				unreachable!(); // ArgGroup should prevent this
-			}
+			let destination = match config.channels.get(&args.channel) {
+				Some(d) => d,
+				None => {
+					return Err(anyhow::anyhow!("Channel not found in {}", cli.config.display()));
+				}
+			};
 
 			let url = format!("https://api.telegram.org/bot{}/sendMessage", bot_token);
-			let params = [("chat_id", format!("{chat_id}")), ("text", args.message.join(" "))];
+			let mut params = vec![("text", args.message.join(" "))];
+			params.extend(destination.destination_params());
 			let client = reqwest::Client::new();
 			let res = client.post(&url).form(&params).send().await?;
 
-			println!("{:#?}\nTarget: {chat_id}; Sender: {bot_token}", res.text().await?);
+			println!("{:#?}\nSender: {bot_token}\n{:#?}", res.text().await?, destination);
 		}
 		Commands::BotInfo => {
 			let url = format!("https://api.telegram.org/bot{}/getMe", bot_token);
@@ -79,6 +67,16 @@ async fn main() -> Result<(), Error> {
 			let parsed_json: serde_json::Value = serde_json::from_str(&res.text().await?).expect("Failed to parse JSON");
 			let pretty_json = serde_json::to_string_pretty(&parsed_json).expect("Failed to pretty print JSON");
 			println!("{}", pretty_json);
+		}
+		Commands::ListChannels => {
+			let mut s = String::new();
+			config.channels.iter().for_each(|(k, _)| {
+				if !s.is_empty() {
+					s.push_str(", ");
+				}
+				s.push_str(k);
+			});
+			println!("{s}");
 		}
 	};
 

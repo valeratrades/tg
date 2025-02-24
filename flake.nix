fb31009c2a1fabf25085182098ea805a895195b0
valeratrades <valeratrades@gmail.com>
2025-02-24 23:18:29 +0000

    _

--- flake.nix
+++ flake.nix
@@ -8,94 +8,114 @@
   };
 
   outputs = { self, nixpkgs, rust-overlay, flake-utils, pre-commit-hooks, v-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = builtins.trace "flake.nix sourced" [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = builtins.trace "flake.nix sourced" [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
 
-        pre-commit-check = pre-commit-hooks.lib.${system}.run (v-utils.files.preCommit { inherit pkgs; });
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        pname = manifest.name;
-        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+          pre-commit-check = pre-commit-hooks.lib.${system}.run (v-utils.files.preCommit { inherit pkgs; });
+          manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+          pname = manifest.name;
+          stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
 
-        workflowContents = v-utils.ci { inherit pkgs; lastSupportedVersion = "nightly-2025-01-16"; jobsErrors = [ "rust-tests" ]; jobsWarnings = [ "rust-doc" "rust-clippy" "rust-machete" "rust-sort" "tokei" ]; };
-        readme = v-utils.readme-fw { inherit pkgs pname; lastSupportedVersion = "nightly-1.86"; rootDir = ./.; licenses = [{ name = "Blue Oak 1.0.0"; outPath = "LICENSE"; }]; badges = [ "msrv" "crates_io" "docs_rs" "loc" "ci" ]; };
+          workflowContents = v-utils.ci { inherit pkgs; lastSupportedVersion = "nightly-2025-01-16"; jobsErrors = [ "rust-tests" ]; jobsWarnings = [ "rust-doc" "rust-clippy" "rust-machete" "rust-sort" "tokei" ]; };
+          readme = v-utils.readme-fw { inherit pkgs pname; lastSupportedVersion = "nightly-1.86"; rootDir = ./.; licenses = [{ name = "Blue Oak 1.0.0"; outPath = "LICENSE"; }]; badges = [ "msrv" "crates_io" "docs_rs" "loc" "ci" ]; };
+        in
+        {
+          packages =
+            let
+              rust = (pkgs.rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml);
+              rustc = rust;
+              cargo = rust;
+              rustPlatform = pkgs.makeRustPlatform {
+                inherit rustc cargo stdenv;
+              };
+            in
+            {
+              default = rustPlatform.buildRustPackage rec {
+                inherit pname;
+                version = manifest.version;
 
-        service = ''
-          [Unit]
-          Description=TG Server Service
-          After=network.target
+                buildInputs = with pkgs; [
+                  openssl.dev
+                ];
+                nativeBuildInputs = with pkgs; [ pkg-config ];
+                env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
-          [Service]
-          Type=simple
-          User=YOUR_USERNAME
-          ExecStart=/path/to/tg server
-          Restart=on-failure
-
-          [Install]
-          WantedBy=multi-user.target'';
-      in
-      {
-        packages =
-          let
-            rust = (pkgs.rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml);
-            rustc = rust;
-            cargo = rust;
-            rustPlatform = pkgs.makeRustPlatform {
-              inherit rustc cargo stdenv;
+                cargoLock.lockFile = ./Cargo.lock;
+                src = pkgs.lib.cleanSource ./.;
+              };
             };
-          in
-          {
-            default = rustPlatform.buildRustPackage rec {
-              inherit pname;
-              version = manifest.version;
 
-              buildInputs = with pkgs; [
-                openssl.dev
-              ];
-              nativeBuildInputs = with pkgs; [ pkg-config ];
-              env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          devShells.default = with pkgs; mkShell {
+            inherit stdenv;
+            shellHook =
+              pre-commit-check.shellHook +
+              ''
+                mkdir -p ./.github/workflows
+                rm -f ./.github/workflows/errors.yml; cp ${workflowContents.errors} ./.github/workflows/errors.yml
+                rm -f ./.github/workflows/warnings.yml; cp ${workflowContents.warnings} ./.github/workflows/warnings.yml
 
-              cargoLock.lockFile = ./Cargo.lock;
-              src = pkgs.lib.cleanSource ./.;
+                cp -f ${v-utils.files.licenses.blue_oak} ./LICENSE
+
+                cargo -Zscript -q ${v-utils.hooks.appendCustom} ./.git/hooks/pre-commit
+                cp -f ${(v-utils.hooks.treefmt) {inherit pkgs;}} ./.treefmt.toml
+                cp -f ${(v-utils.hooks.preCommit) { inherit pkgs pname; }} ./.git/hooks/custom.sh
+
+                mkdir -p ./.cargo
+                cp -f ${(v-utils.files.rust.config {inherit pkgs;})} ./.cargo/config.toml
+                cp -f ${(v-utils.files.rust.toolchain {inherit pkgs; toolchain = "nightly";})} ./.cargo/rust-toolchain.toml
+                cp -f ${(v-utils.files.rust.rustfmt {inherit pkgs;})} ./rustfmt.toml
+                cp -f ${(v-utils.files.rust.deny {inherit pkgs;})} ./deny.toml
+                cp -f ${(v-utils.files.gitignore { inherit pkgs; langs = ["rs"];})} ./.gitignore
+
+                cp -f ${readme} ./README.md
+              '';
+
+            packages = [
+              mold-wrapped
+              openssl
+              pkg-config
+              (rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml)
+            ] ++ pre-commit-check.enabledPackages;
+          };
+        }
+      ) // {
+      #good ref: https://github.com/NixOS/nixpkgs/blob/04ef94c4c1582fd485bbfdb8c4a8ba250e359195/nixos/modules/services/audio/navidrome.nix#L89
+      nixosModules.default = { config, lib, pkgs, ... }:
+        let
+          inherit (lib) mkEnableOption mkOption mkIf;
+          inherit (lib.types) package;
+          cfg = config.services.tg-server;
+        in
+        {
+          options.services.tg-server = {
+            enable = mkEnableOption "TG Server Service";
+
+            package = mkOption {
+              type = package;
+              default = self.packages.${pkgs.system}.default;
+              description = "The tg-server package to use.";
             };
           };
 
-        devShells.default = with pkgs; mkShell {
-          inherit stdenv;
-          shellHook =
-            pre-commit-check.shellHook +
-            ''
-              mkdir -p ./.github/workflows
-              rm -f ./.github/workflows/errors.yml; cp ${workflowContents.errors} ./.github/workflows/errors.yml
-              rm -f ./.github/workflows/warnings.yml; cp ${workflowContents.warnings} ./.github/workflows/warnings.yml
+          config = mkIf cfg.enable {
+            systemd.user.services.tg-server = {
+              description = "TG Server Service";
+              wantedBy = [ "default.target" ];
+              after = [ "network.target" ];
 
-              cp -f ${v-utils.files.licenses.blue_oak} ./LICENSE
-
-              cargo -Zscript -q ${v-utils.hooks.appendCustom} ./.git/hooks/pre-commit
-              cp -f ${(v-utils.hooks.treefmt) {inherit pkgs;}} ./.treefmt.toml
-              cp -f ${(v-utils.hooks.preCommit) { inherit pkgs pname; }} ./.git/hooks/custom.sh
-
-              mkdir -p ./.cargo
-              cp -f ${(v-utils.files.rust.config {inherit pkgs;})} ./.cargo/config.toml
-              cp -f ${(v-utils.files.rust.toolchain {inherit pkgs; toolchain = "nightly";})} ./.cargo/rust-toolchain.toml
-              cp -f ${(v-utils.files.rust.rustfmt {inherit pkgs;})} ./rustfmt.toml
-              cp -f ${(v-utils.files.rust.deny {inherit pkgs;})} ./deny.toml
-              cp -f ${(v-utils.files.gitignore { inherit pkgs; langs = ["rs"];})} ./.gitignore
-
-              cp -f ${readme} ./README.md
-            '';
-
-          packages = [
-            mold-wrapped
-            openssl
-            pkg-config
-            (rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml)
-          ] ++ pre-commit-check.enabledPackages;
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = "${cfg.package}/bin/tg server";
+                Restart = "on-failure";
+              };
+            };
+          };
         };
-      }
-    );
+    };
 }
 

908971ead36f21b3389498e6d7f380bd4f90d1c8
Valera <valeratrades@gmail.com>
2025-12-18 21:59:51 +0000

    _

--- build.rs
+++ build.rs
@@ -1,7 +1,14 @@
 use std::process::Command;
 
 fn main() {
-	let output = Command::new("git").args(["rev-parse", "--short", "HEAD"]).output().unwrap();
-	let git_hash = String::from_utf8(output.stdout).unwrap();
-	println!("cargo:rustc-env=GIT_HASH={}", git_hash.trim());
+	let git_hash = Command::new("git")
+		.args(["rev-parse", "--short", "HEAD"])
+		.output()
+		.ok()
+		.and_then(|o| if o.status.success() { Some(o.stdout) } else { None })
+		.and_then(|stdout| String::from_utf8(stdout).ok())
+		.map(|s| s.trim().to_string())
+		.unwrap_or_else(|| "unknown".to_string());
+
+	println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
